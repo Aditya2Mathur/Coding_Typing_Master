3c595658d194abbe86a36dff8067032363a80e4a
Aditya Mathur <adiasfreelancer@gmail.com>
2024-06-20 09:02:29 +0530

    update info page

--- src/Screens/InfoPage.js
+++ src/Screens/InfoPage.js
@@ -13,7 +13,7 @@ function InfoPage() {
               <p className='text-xs lg:text-2xl xl:text-2xl md:text-xl'>
               Coding Typing Master is a ReactJS-based web app designed to improve typing skills, tailored for computer science students. It features customizable themes, real-time typing analysis, and a diverse library of programming keywords and logic-building exercises sourced from ChatGPT, offering an engaging environment for users to enhance both their typing proficiency and programming knowledge.</p>
             </div>
-            <p className='text-xl lg:text-4xl sm:text-2xl'>Built by <br></br> <a href='https://github.com/Aditya2Mathur' className='text-skin-base underline'>Aditya Mathur, Aditya Kumar <br></br> Shashank Dixit, Abhishek Kumar</a>.</p>
+            <p className='text-xl lg:text-2xl sm:text-2xl'>Built by <br></br> <a href='https://github.com/Aditya2Mathur' className='text-skin-base underline'>Aditya Kumar Mathur</a>.</p>
           </div>
         </div>
       </section>
